9755eacc5684b04d4176320c36cafd541cd5a667
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2020-07-20 14:05:54 +0530

    Remove Linked List Elements

---
+++ Day20.cpp
@@ -0,0 +1,33 @@
+/* QUESTION: Remove all elements from a linked list of integers that have value val.
+
+Example:
+
+Input:  1->2->6->3->4->5->6, val = 6
+Output: 1->2->3->4->5
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* newnode = new ListNode(0);
+        newnode->next = head;
+        head = newnode;
+        while(newnode->next != NULL) {
+            if(newnode->next->val == val) {
+                newnode-> next = newnode->next->next;
+            }
+            else
+                newnode = newnode->next;
+        }
+        return head->next;
+        
+    }
+};
